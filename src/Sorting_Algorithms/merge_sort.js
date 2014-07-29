1d1df30dbca85ab6122c32bfee69e30c925fded4
Dima Maleev <diwingless@gmail.com>
2014-07-30 00:16:13 +0300

    small refactoring of merge sort

--- src/Sorting_Algorithms/merge_sort.js
+++ src/Sorting_Algorithms/merge_sort.js
@@ -1,49 +1,45 @@
-function fillnumbers(amount){
-	for (var i = 0 ; i < amount; i++){
-		num[i] = Math.floor(Math.random() * 100 + 1);
-	}
-}
+var SORTING_ALGORITHMS = SORTING_ALGORITHMS || {};
 
-function setvals(val){
-	num.push(val);
-}
+(function(){
+	
+	SORTING_ALGORITHMS.merge_sort = function(array){
+		
 
+		function merge(left, right){
+			var result = [];
+			var il = 0;
+			var ir = 0;
 
-function merge(left, right){
-	var result = [];
-	var il = 0;
-	var ir = 0;
-
-	while (il < left.length && ir < right.length){
-		if (left[il] < right[ir]){
-			result.push(left[il++]);
-		} else {
-			result.push(right[ir++]);
+			while (il < left.length && ir < right.length){
+				if (left[il] < right[ir]){
+					result.push(left[il++]);
+				} else {
+					result.push(right[ir++]);
+				}
+			}
+			return result.concat(left.slice(il)).concat(right.slice(ir));
 		}
+
+		function merge_sort(items){
+			if (items.length < 2){
+				return items;
+			}
+
+			var middle = Math.floor(items.length / 2);
+
+			var left = items.slice(0, middle);
+			var right = items.slice(middle);
+
+
+			var params = merge(merge_sort(left), merge_sort(right)); 
+
+			params.unshift(0, items.length);
+			items.splice.apply(items, params);
+
+			return items;
+		}
+
+		return merge_sort(array);
 	}
-
-	console.log(result);
-
-	return result.concat(left.slice(il)).concat(right.slice(ir));
-}
-
-function mergeSort(items){
-
-	if (items.length < 2){
-		return items;
-	}
-
-	var middle = Math.floor(items.length / 2);
-
-	var left = items.slice(0, middle);
-	var right = items.slice(middle);
-
-
-	var params = merge(mergeSort(left), mergeSort(right)); 
-
-	params.unshift(0, items.length);
-	items.splice.apply(items, params);
-
-	return items;
-}
+})();
 
